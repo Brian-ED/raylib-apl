80f3b28abcb6a4651ba80c7a87ab67d722e19ecd
Brian-ED <brianellingsgaard9@gmail.com>
2024-07-18 15:24:50 +0100

    Huggge progress Auto padding of structs now working fixed 32 bit floats being weird values, fixed by fixing struct padding. Added more testing with zig, since it's easy to compile and it's structs are easy to write. shapes example finally drawing circle!! return values haven't been done yet

---
+++ breakapl/main.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+const color = packed struct {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+};
+
+export fn sayFloat(int1: i32, int2: i32, float1: f32, float2: f32, color1: color, int3:i64) void {
+    // Comments in Zig start with "//" and end at the next LF byte (end of line).
+    // The line below is a comment and won't be executed.
+
+    if (std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{},
+    )) |file| {
+        const fmtArgs = .{ int1, int2, int3, @as(u32, @bitCast(float1)), float1, @as(u32, @bitCast(float2)), float2, color1 };
+        const fmtStr = "int1: {} int2: {} int3: {}\nfloat1: {}, {} float2: {}, {}\ncolor1: {} end\n";
+        const out = std.fmt.format(file.writer(), fmtStr, fmtArgs);
+        if (out) {} else |err| {
+            print("{}", .{err});
+        }
+        file.close();
+    } else |err| {
+        print("{}", .{err});
+    }
+}
+
+// zig build
+// -rdynamic
+// -dynamic
+
+
+const someone = extern struct {
+    a: u64,
+    b:extern struct {a:u64, b:u32},
+    c:u8
+};
+
+export fn sayI8(int1: i8, int2: i8, int3:i8, int4:i8, int5:i8, s:someone) void {
+    // Comments in Zig start with "//" and end at the next LF byte (end of line).
+    // The line below is a comment and won't be executed.
+
+    if (std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{},
+    )) |file| {
+        const fmtArgs = .{ int1, int2, int3, int4, int5, s};
+        const fmtStr = "int1: {} int2: {} int3: {} int4: {} int5: {}\ns: {} end\n";
+        const out = std.fmt.format(file.writer(), fmtStr, fmtArgs);
+        if (out) {} else |err| {
+            print("{}", .{err});
+        }
+        file.close();
+    } else |err| {
+        print("{}", .{err});
+    }
+}
+
+
+const padded = extern struct {
+    a: u8,
+    b: u64,
+};
+
+export fn pad(s:padded) void {
+    // Comments in Zig start with "//" and end at the next LF byte (end of line).
+    // The line below is a comment and won't be executed.
+
+    if (std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{},
+    )) |file| {
+        const fmtArgs = .{s};
+        const fmtStr = "s: {} end\n";
+        const out = std.fmt.format(file.writer(), fmtStr, fmtArgs);
+        if (out) {} else |err| {
+            print("{}", .{err});
+        }
+        file.close();
+    } else |err| {
+        print("{}", .{err});
+    }
+}
---
+++ breakapl/test.dyalog
@@ -0,0 +1,8 @@
+⍝⎕NA '/mnt/0AD47A53D47A414D/Users/brian/Persinal/Scripts/APL/raylib-apl/breakapl/zig-out/lib/libbreakapl.so|sayFloat I4 I4 F4 F4 {U1 U1 U1 U1 U4} U8'
+⍝ sayFloat 1 2 3 4 (5 6 7 8 9) 10
+⍝ ⎕NA '/mnt/0AD47A53D47A414D/Users/brian/Persinal/Scripts/APL/raylib-apl/breakapl/zig-out/lib/libbreakapl.so|sayI8 I1 I1 I1 I1 I1 {U8 {U8 U4 U4} U1 U4 U2 U1}'
+⍝ sayI8 1 2 3 4 5 (6 (7 8 0) 9 0 0 0)
+⍝ ⎕←0
+⎕NA '/mnt/0AD47A53D47A414D/Users/brian/Persinal/Scripts/APL/raylib-apl/breakapl/zig-out/lib/libbreakapl.so|pad {U1 U4 U2 U1 U8}'
+pad ,⊂(1 0 0 0 2)
+⎕←0
