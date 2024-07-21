2931b79b89f74d14987fd4d0d1d34212f1a296a4
Brian-ED <brianellingsgaard9@gmail.com>
2024-07-21 11:30:19 +0100

    updated zig and am testing structs with apl

--- breakapl/build.zig
+++ breakapl/build.zig
@@ -19,16 +19,13 @@ pub fn build(b: *std.Build) void {
         .name = "breakapl",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "main.zig" },
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
     b.installArtifact(lib);
 }
-
-
